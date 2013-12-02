baba9d61b4dd3e31b1c6f79d497e835dfcca3f35
Santosh Suresh <santoshdeshpande@gmail.com>
2013-12-02 17:29:36 +0530

    Moved tests to the right folder

---
+++ brandclub/core/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.db import IntegrityError
+from django.test import TestCase
+from ..models import Brand
+
+
+class BrandTestCase(TestCase):
+
+    def test_check(self):
+        #self.assertEquals(1, 2)
+        Brand.objects.create(name="B1", description="", logo="")
+        with self.assertRaises(IntegrityError):
+            Brand.objects.create(name="B1",description="", logo="")
+
+    def test_image_tag_returns_proper_url(self):
+        brand = Brand.objects.create(name="B1", description="Some desc", logo="/home/test/image.jpg")
+        self.assertEquals(u"<img src='/home/test/image.jpg'>", brand.image_tag())
