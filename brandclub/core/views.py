10c5a9b8d7231f583e61ffff56500fcfe1e95d84
Santosh Suresh <santoshdeshpande@gmail.com>
2013-12-17 04:32:28 +0530

    Working version with bootstrap ui

--- brandclub/core/views.py
+++ brandclub/core/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from models import Store, Brand, Content, Device, Cluster
-from middleware import ClusterDeviceDetectionMiddleware
+from django.shortcuts import get_object_or_404
 from django.template.loader import get_template
 from django.template import Context
-# Create your views here.
+
+from .models import Brand, Cluster
 
 
 def slug_view(request, slug):
@@ -12,6 +11,6 @@ def slug_view(request, slug):
     home_cluster = get_object_or_404(Cluster, id=cluster_id)
     all_contents = home_cluster.get_all_home_content()
     home_brand = get_object_or_404(Brand, slug_name=slug)
-    t = get_template('cluster_home.html')
-    html = t.render(Context({'contents': all_contents, 'cluster': home_cluster, 'home_brand': home_brand}))
+    t = get_template('home.html')
+    html = t.render(Context({'contents': all_contents, 'cluster': home_cluster, 'brand': home_brand}))
     return HttpResponse(html)
