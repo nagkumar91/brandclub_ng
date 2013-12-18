f0cb769879d6477ca65ade64d2638c967f33f813
Santosh Suresh <santosh.s@telibrahma.com>
2013-12-18 07:42:12 +0000

    Added masonry support for tiling

--- brandclub/brandclub/settings/base.py
+++ brandclub/brandclub/settings/base.py
@@ -164,7 +164,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'core.middleware.ClusterDeviceDetectionMiddleware',
 )
 ########## END MIDDLEWARE CONFIGURATION
