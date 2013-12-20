46392de1ad34c51c3157e68eb89e1e4bae4d0199
Santosh Suresh <santosh.s@telibrahma.com>
2013-12-20 09:05:52 +0000

    Added allowed hosts

--- brandclub/brandclub/settings/production.py
+++ brandclub/brandclub/settings/production.py
@@ -19,7 +19,7 @@ def get_env_setting(setting):
 
 ########## HOST CONFIGURATION
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['beta.brandclub.mobi']
 ########## END HOST CONFIGURATION
 
 ########## EMAIL CONFIGURATION
