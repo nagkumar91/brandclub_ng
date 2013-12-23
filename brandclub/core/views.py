fe8a5c35cd926f41201cb5d88e9fab1192a8a019
Santosh Suresh <santosh.s@telibrahma.com>
2013-12-23 11:39:38 +0000

    Refactored to use get object or None

--- brandclub/core/views.py
+++ brandclub/core/views.py
@@ -1,28 +1,33 @@
+from annoying.functions import get_object_or_None
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
 from .models import Brand, Cluster, Store, Content, SlideShow
 
+
 def slug_view(request, slug):
     cluster_id = request.cluster_id
-    home_cluster = get_object_or_404(Cluster, id=cluster_id)
-    all_contents = home_cluster.get_all_home_content(request.device_id)
-    home_brand = get_object_or_404(Brand, slug_name=slug)
-    context = {'contents': all_contents, 'cluster': home_cluster, 'brand': home_brand}
-    context_instance = RequestContext(request, context)
-    return render_to_response('home.html', context_instance)
+    home_cluster = get_object_or_None(Cluster, id=cluster_id)
+    if home_cluster is not None:
+        all_contents = home_cluster.get_all_home_content(request.device_id)
+        home_brand = get_object_or_None(Brand, slug_name=slug)
+        context = {'contents': all_contents, 'cluster': home_cluster, 'brand': home_brand}
+        context_instance = RequestContext(request, context)
+        return render_to_response('home.html', context_instance)
+    return render_to_response('default.html', {'cluster':cluster_id})
 
 
 def store_home(request, store_id):
-    store = get_object_or_404(Store, id=store_id)
-    brand = get_object_or_404(Brand, id=store.brand_id)
+    store = get_object_or_None(Store, id=store_id)
+    brand = get_object_or_None(Brand, id=store.brand_id)
     contents = Content.active_objects.filter(show_on_home=False, store=store).select_subclasses()
     return render_to_response('store_home.html', {'contents': contents, 'brand': brand, 'store': store})
 
+
 def slideshow(request, ssid):
-    slides = get_object_or_404(SlideShow, id=ssid)
-    return render_to_response('slide_show.html', {'content':slides})
+    slides = get_object_or_None(SlideShow, id=ssid)
+    return render_to_response('slide_show.html', {'content': slides})
+
 
 def display_clusters(request):
-
     return render_to_response('home.html', {})
