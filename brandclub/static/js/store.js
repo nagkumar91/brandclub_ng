26ee9c4c4b73b9941da841fa660059c05afbadd3
Santosh Suresh <santosh.s@telibrahma.com>
2013-12-24 01:16:45 +0000

    Setup popover width

--- brandclub/static/js/store.js
+++ brandclub/static/js/store.js
@@ -4,7 +4,7 @@ $(function($) {
         title : 'Store Location',
         html: true,
         content: function () {
-            return '<a target="_blank" href="'+$(this).data('store-map')+'"><img class="img-responsive" src="' + $(this).data('store-map')+'" /></a>';
+            return '<div class="col-md-6 col-xs-10"><a target="_blank" href="'+$(this).data('store-map')+'"><img class="img-responsive" src="' + $(this).data('store-map')+'" /></a></div>';
         }
     });
 
