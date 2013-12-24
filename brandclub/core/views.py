5397bb15c1627a4b17fcfb24ddb2fb0c41c89237
Santosh Suresh <santosh.s@telibrahma.com>
2013-12-24 00:39:26 +0000

    Fixed to use slug instead of store id

--- brandclub/core/views.py
+++ brandclub/core/views.py
@@ -20,7 +20,7 @@ def slug_view(request, slug):
 def store_home(request, slug):
     store = get_object_or_None(Store, slug_name=slug)
     contents = Content.active_objects.filter(show_on_home=False, store=store).select_subclasses()
-    return render_to_response('store_home.html', {'contents': contents, 'store': store})
+    return render_to_response('store_home.html', {'contents': contents, 'store': store, 'brand': store.brand})
 
 
 def slideshow(request, ssid):
