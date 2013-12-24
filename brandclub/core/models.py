ec352d1b58d4eabefe492e76a95bb4f19c535f45
Santosh Suresh <santosh.s@telibrahma.com>
2013-12-24 00:12:35 +0000

    Removed default -1 for store slug and fixed the navbar issue

--- brandclub/core/models.py
+++ brandclub/core/models.py
@@ -147,7 +147,7 @@ class Cluster(TimeStampedModel):
 
 class Store(TimeStampedModel):
     name = models.CharField(max_length=100)
-    slug_name = models.SlugField(max_length=100, default="-1")
+    slug_name = models.SlugField(max_length=100, default="")
     description = models.TextField(null=True, blank=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
