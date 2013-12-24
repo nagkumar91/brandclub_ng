ac9a2fab7bf337518ec8159f1eaf86f06433565d
Santosh Suresh <santosh.s@telibrahma.com>
2013-12-24 08:17:03 +0000

    Removed unused imports

--- brandclub/brandclub/urls.py
+++ brandclub/brandclub/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
