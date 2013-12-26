a5e13b1954d2f0521e399d111f96d567cba2ec4b
Santosh Suresh <santosh.s@telibrahma.com>
2013-12-26 07:25:32 +0000

    Some cleanup

--- brandclub/core/views.py
+++ brandclub/core/views.py
@@ -4,10 +4,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.core.cache import cache
-from django.views.decorators.csrf import csrf_exempt
 from .forms import FeedbackForm
 
-from .models import Brand, Cluster, Store, Content, SlideShow, Device, StoreFeedback
+from .models import Brand, Cluster, Store, Content, SlideShow, Device
 
 
 def slug_view(request, slug):
@@ -49,36 +48,6 @@ def redirect_to_outside(request):
     url = request.GET.get('href', 'http://www.google.com')
     return HttpResponseRedirect(url)
 
-
-def slideshow(request, ssid):
-    slides = get_object_or_None(SlideShow, id=ssid)
-    return render_to_response('slide_show.html', {'content': slides})
-
-
-def display_clusters(request):
-    return render_to_response('home.html', {})
-
-
-def submit_feedback(request):
-    if request.method == 'POST':
-        form = FeedbackForm(request.POST)
-        form.is_valid()
-        # form.clean()
-        name = request.POST["name"]
-        phone_number = request.POST["phone_number"]
-        email_id = request.POST["email_id"]
-        message = request.POST["message"]
-        store = request.POST["store"]
-        store_obj = get_object_or_404(Store, pk=store)
-        sfb = StoreFeedback(name=name, phone_number=phone_number,
-                            email_id=email_id, message=message, store=store_obj)
-        # sfb.save()
-        url = '/home/%s' % store_obj.slug_name
-        return HttpResponseRedirect(url)
-    return render_to_response("success.html")
-
-
-# @csrf_exempt
 def store_feedback(request, slug):
     store = get_object_or_404(Store, slug_name=slug)
     form = FeedbackForm()
