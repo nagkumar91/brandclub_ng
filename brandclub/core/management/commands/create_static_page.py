30b4b7b7b247e5774a8111403c3fc5c4dfb4ac15
nagkumar <nagkumar@telibrahma.com>
2014-01-14 15:51:26 +0530

    Static pages will be created for wallpaper, slideshow, cluster info, store info

--- brandclub/core/management/commands/create_static_page.py
+++ brandclub/core/management/commands/create_static_page.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.core.management import BaseCommand
-from core.models import Cluster, Store, Brand
+from core.models import Cluster, Store, Brand, OrderedStoreContent, ContentType
 from django.test import Client
 import os
 
@@ -10,11 +10,22 @@ class Command(BaseCommand):
     def _generate_page(self, dir, cluster_id, device_id, store):
         store_slug = store.slug_name
         self._generate_main_page(store_slug, cluster_id, device_id, dir)
-        cluster = Cluster.objects.get(pk = cluster_id)
+        cluster = Cluster.objects.get(pk=cluster_id)
         stores = cluster.stores.all()
         for cluster_store in stores:
             slug = cluster_store.slug_name
             self._generate_store_home_page(slug, cluster_id, device_id, dir)
+            self._generate_feedback_forms(slug, cluster_id, device_id, dir)
+            self._generate_cluster_info(slug, cluster_id, device_id, dir)
+            self._generate_store_info(slug, cluster_id, device_id, dir)
+            contents = OrderedStoreContent.objects.filter(store=cluster_store)
+            ctype_slideshow = ContentType.objects.get_or_create(name="Slide Show")
+            ctype_wallpaper = ContentType.objects.get_or_create(name="Wallpaper")
+            for individual_content in contents:
+                if individual_content.content_type is ctype_slideshow:
+                    self._generate_slideshow(individual_content.id, cluster_id, device_id, dir)
+                if individual_content.content_type is ctype_wallpaper:
+                    self._generate_wallpapers(individual_content.id, cluster_id, device_id, dir)
 
     def _generate_main_page(self, slug, cluster_id, device_id, static_dir):
         client = Client()
@@ -22,6 +33,26 @@ class Command(BaseCommand):
         response = client.get(page, **{'X_CLUSTER_ID': cluster_id, 'X_DEVICE_ID': device_id})
         print "Generating %s and got code %d " % (slug, response.status_code)
         output_file = "/%s/%s" % (static_dir, slug)
+        with open(output_file, 'w') as f:
+            f.write(response.content)
+            f.close()
+
+    def _generate_store_info(self, slug, cluster_id, device_id, static_dir):
+        client = Client()
+        page = "/si/%s/" % slug
+        response = client.get(page, **{'X_CLUSTER_ID': cluster_id, 'X_DEVICE_ID': device_id})
+        print "Generating %s and got code %d " % (slug, response.status_code)
+        output_file = "/%s/%s" % (static_dir, slug)
+        with open(output_file, 'w') as f:
+            f.write(response.content)
+            f.close()
+
+    def _generate_cluster_info(self, slug, cluster_id, device_id, static_dir):
+        client = Client()
+        page = "/ci/"
+        response = client.get(page, **{'X_CLUSTER_ID': cluster_id, 'X_DEVICE_ID': device_id})
+        print "Generating %s and got code %d " % (slug, response.status_code)
+        output_file = "/%s/" % static_dir
         with open(output_file, 'w') as f:
             f.write(response.content)
             f.close()
@@ -35,6 +66,45 @@ class Command(BaseCommand):
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
         output_file = "/%s/%s" % (output_dir, slug)
+        with open(output_file, 'w') as f:
+            f.write(response.content)
+            f.close()
+
+    def _generate_feedback_forms(self, slug, cluster_id, device_id, static_dir):
+        client = Client()
+        page = "/feedback/%s/" % slug
+        response = client.get(page, **{'X_CLUSTER_ID': cluster_id, 'X_DEVICE_ID': device_id})
+        print "Generating %s and got code %d " % (page, response.status_code)
+        output_dir = "%s/home" % static_dir
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_file = "/%s/%s" % (output_dir, slug)
+        with open(output_file, 'w') as f:
+            f.write(response.content)
+            f.close()
+
+    def _generate_slideshow(self, ssid, cluster_id, device_id, static_dir):
+        client = Client()
+        page = "/slideshow/%s/" % ssid
+        response = client.get(page, **{'X_CLUSTER_ID': cluster_id, 'X_DEVICE_ID': device_id})
+        print "Generating %s and got code %d " % (page, response.status_code)
+        output_dir = "%s/home" % static_dir
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_file = "/%s/%s" % (output_dir, ssid)
+        with open(output_file, 'w') as f:
+            f.write(response.content)
+            f.close()
+
+    def _generate_wallpapers(self, wid, cluster_id, device_id, static_dir):
+        client = Client()
+        page = "/wallpaper/%s/" % wid
+        response = client.get(page, **{'X_CLUSTER_ID': cluster_id, 'X_DEVICE_ID': device_id})
+        print "Generating %s and got code %d " % (page, response.status_code)
+        output_dir = "%s/home" % static_dir
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_file = "/%s/%s" % (output_dir, wid)
         with open(output_file, 'w') as f:
             f.write(response.content)
             f.close()
