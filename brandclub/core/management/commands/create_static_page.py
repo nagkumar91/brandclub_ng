27550c9ea128ac8b5781520d08540a7dbd3534cc
Santosh Suresh <santosh.s@telibrahma.com>
2014-01-14 13:37:14 +0000

    Refactored code

--- brandclub/core/management/commands/create_static_page.py
+++ brandclub/core/management/commands/create_static_page.py
@@ -20,25 +20,24 @@ class Command(BaseCommand):
     def _generate_page(self, dir, cluster_id, device_id, store):
         print "Generating contents for cluster %s and device %s " % (cluster_id, device_id)
         print "==================================================================="
-        store_slug = store.slug_name
-        self._generate_main_page(store_slug, cluster_id, device_id, dir)
+        self._generate_main_page(store.slug_name, cluster_id, device_id, dir)
+        self._generate_cluster_info(cluster_id, device_id, dir)
         cluster = Cluster.objects.get(pk=cluster_id)
         stores = cluster.stores.all()
         for cluster_store in stores:
             slug = cluster_store.slug_name
             self._generate_store_home_page(slug, cluster_id, device_id, dir)
-        self._generate_feedback_forms(slug, cluster_id, device_id, dir)
-        self._generate_cluster_info(slug, cluster_id, device_id, dir)
-        self._generate_store_info(slug, cluster_id, device_id, dir)
-        contents = OrderedStoreContent.objects.filter(store=cluster_store)
-        ctype_slideshow = ContentType.objects.get_or_create(name="Slide Show")
-        ctype_wallpaper = ContentType.objects.get_or_create(name="Wallpaper")
-        for individual_content in contents:
-            content = individual_content.content
-            if content.content_type is ctype_slideshow:
-                self._generate_slideshow(content.id, cluster_id, device_id, dir)
-            if content.content_type is ctype_wallpaper:
-                self._generate_wallpapers(content.id, cluster_id, device_id, dir)
+            self._generate_feedback_forms(slug, cluster_id, device_id, dir)
+            self._generate_store_info(slug, cluster_id, device_id, dir)
+            contents = OrderedStoreContent.objects.filter(store=cluster_store)
+            ctype_slideshow = ContentType.objects.get_or_create(name="Slide Show")
+            ctype_wallpaper = ContentType.objects.get_or_create(name="Wallpaper")
+            for individual_content in contents:
+                content = individual_content.content
+                if content.content_type is ctype_slideshow:
+                    self._generate_slideshow(content.id, cluster_id, device_id, dir)
+                if content.content_type is ctype_wallpaper:
+                    self._generate_wallpapers(content.id, cluster_id, device_id, dir)
         print "==================================================================="
 
     def _generate_main_page(self, slug, cluster_id, device_id, static_dir):
@@ -57,7 +56,7 @@ class Command(BaseCommand):
             f.write(response.content)
             f.close()
 
-    def _generate_cluster_info(self, slug, cluster_id, device_id, static_dir):
+    def _generate_cluster_info(self, cluster_id, device_id, static_dir):
         page = "/ci/"
         response = self.generate_response(cluster_id, device_id, page)
         output_file = "/%s/ci" % static_dir
@@ -113,8 +112,10 @@ class Command(BaseCommand):
         stores = Store.objects.all()
         for store in stores:
             cluster = store.cluster.id
+            print "Store is %s" % store.name
             for device in store.devices.all():
                 device_id = "%s" % device.device_id
+                print "Device is %s" % device_id
                 static_dir = os.path.join(settings.CONTENT_CACHE_DIRECTORY, "content", device_id)
                 os.makedirs(static_dir)
                 self._generate_page(static_dir, cluster, device.device_id, store)
