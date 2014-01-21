939ce31ea25fe24cc82abb7304d972698aece7ee
nagkumar <nagkumar@telibrahma.com>
2014-01-21 17:30:31 +0530

    Multiple slideshows in cluster home fixed

--- brandclub/static/js/store.js
+++ brandclub/static/js/store.js
@@ -1,11 +1,17 @@
 $(function($) {
-    var links = $("#links a");
-    blueimp.Gallery(links, {
-       container: "#slide-show-carousel",
-       carousel: true,
-       slidesContainer:'div',
-       slideClass:'slide',
-        urlProperty:'href',
-        fullScreen:true
+
+    var slideshows = $(".slideshow");
+    $.each(slideshows, function(index, element){
+        var links = $(".links a", $(element));
+        var slideshowId = $(element).attr('data-slideshow-id');
+        blueimp.Gallery(links, {
+           container: "#slide-show-carousel-"+slideshowId,
+           carousel: true,
+           slidesContainer:'div',
+           slideClass:'slide',
+            urlProperty:'href',
+            fullScreen:true
+        });
+
     });
 }(jQuery));
