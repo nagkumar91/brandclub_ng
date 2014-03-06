e7583c785d97c2c9c30de8d71cb09d6e8d9b2707
Santosh Suresh <santoshdeshpande@gmail.com>
2014-03-06 12:03:16 +0530

    Fixed the aws keys

--- brandclub/brandclub/settings/base.py
+++ brandclub/brandclub/settings/base.py
@@ -339,4 +339,4 @@ CONTENT_ID_MAPPING = {-1: "Cluster Home",
                       -6: "Offers in Cluster"}
 CONTENT_ID_ARRAY = [-1, -2, -3, -4, -5, -6]
 LOG_SAVE_PATH = "/tmp/bc_logs_csv/"
-AWS_BUCKET_NAME = ''
+AWS_BUCKET_NAME = 'tib.bcng.content'
