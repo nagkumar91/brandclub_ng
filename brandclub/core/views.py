501a5e092e5e8ea4889a460767c0728d23e8fcd2
Nagkumar <nagkumar@live.com>
2014-06-06 13:20:19 +0530

    Printing messages for debug

--- brandclub/core/views.py
+++ brandclub/core/views.py
@@ -452,15 +452,19 @@ def create_bc_user(request):
         if mac_address is not '' and user_unique_id is not '':
             user_obj = BrandClubUser.objects.get(mac_id=mac_address)
             if user_obj is not None:
+                print "BCU exists"
                 return
         else:
             user_obj = BrandClubUser.objects.get(user_unique_id=user_unique_id)
             if user_obj is not None:
+                print "BCU exists"
                 return
     except ObjectDoesNotExist:
         try:
+            print "Creating BCU"
             user_obj = BrandClubUser(mac_id=mac_address, user_unique_id=user_unique_id, coupon_generated_at=store)
             user_obj.save()
+            print "%s-%s-%s" % (user_obj.user_id, user_obj.mac_id, user_obj.user_unique_id)
         except IntegrityError:
             pass
 
